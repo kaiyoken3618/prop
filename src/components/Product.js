806d0cdb9f32ef6825529dbb1f1d2e980d8cc8cb
kaiyoken3618 <sakibenam3618@gmail.com>
2022-10-04 15:11:02 +0600

    Product List app

---
+++ src/components/Product.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Product = (props) => {
+     
+  const {title, price, description,img, rating } = props;
+  return (
+      <article className="product">
+      <img src={img} alt="product" />
+      <div className="product__details">
+        <h4 className="product__title">{title}</h4>
+        <p className='product__price'>Price: ${price}</p>
+        <p className='product__rate'>Rating: {rating}/5</p>
+        <p className="product__desc">Description: {description}</p>
+        <button className="product__btn btn">Add to cart</button>
+      </div>
+    </article>
+  );
+};
+
+export default Product;
